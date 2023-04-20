a52d902428197e2818aaedcfc4581f101f932d37
Charles Jackson <charles@jacksoncharles.co.uk>
2023-04-20 16:57:47 +0100

    incremental backup

--- js/init.js
+++ js/init.js
@@ -1,8 +1,5 @@
-(function($){
-  $(function(){
-
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
-
-  }); // end of document ready
+(function ($) {
+    $(function () {
+        $(".parallax").parallax();
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
