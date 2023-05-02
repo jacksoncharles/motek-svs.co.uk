9c4f82c55c2339b363064850239862e1be122aa1
Charles Jackson <charles@jacksoncharles.co.uk>
2023-05-02 17:17:31 +0100

    SEO changes, hidden text

--- js/init.js
+++ js/init.js
@@ -2,4 +2,9 @@
     $(function () {
         $(".parallax").parallax();
     }); // end of document ready
+    $(function () {
+        $("#motek").click(function () {
+            $("#seo").toggle();
+        });
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
